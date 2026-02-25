91c7d8ae3660026107f64301b8fb3a551124e7eb
Heng Lu <henglu@microsoft.com>
2026-02-25 10:37:41 +0800

    docs: document beta API import using ?api-version=beta query parameter

--- examples/resources/msgraph_resource/import.sh
+++ examples/resources/msgraph_resource/import.sh
@@ -1,3 +1,6 @@
 # MSGraph resource can be imported using the resource id, e.g.
 terraform import msgraph_resource.servicePrincipal /servicePrincipals/00000000-0000-0000-0000-000000000000
 terraform import msgraph_resource.member /groups/group-id/members/$ref/00000000-0000-0000-0000-000000000000
+
+# For resources that use the beta API, append ?api-version=beta to the import ID:
+terraform import msgraph_resource.settings '/settings/00000000-0000-0000-0000-000000000000?api-version=beta'
